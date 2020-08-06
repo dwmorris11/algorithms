5e150e3baef34aea1dc82601110e0a7f673ddaa5
Dustin Morris <dustinnuc@gmail.com>
2020-08-05 20:36:09 -0700

    singlenumber

---
+++ singlenumber_easy.js
@@ -0,0 +1,15 @@
+var singleNumber = function(nums) {
+  let obj = {};
+  for(let i = 0; i < nums.length; i++) {
+     if(obj[nums[i]] === undefined) {
+         obj[nums[i]] = 1;
+     }else{
+         obj[nums[i]]++;
+     }
+ }
+  for(let key in obj){
+      if(obj[key]===1){
+          return key;
+      }
+  }
+};
