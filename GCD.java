3538e2856ce95d76beb5f884f997488d51005240
Dustin Morris <dustinnuc@gmail.com>
2020-10-12 16:03:19 -0700

    Euclids Algorithm in Java

---
+++ GCD.java
@@ -0,0 +1,15 @@
+class GCD {
+  public static void main (String [] args) {
+    System.out.println(euclid(121,240));
+  }
+  public static int euclid(int m, int n) {
+    int r = m%n;
+    if(r != 0) {
+      m = n;
+      n = r;
+      return euclid(m, n);
+    }
+    return n;
+  }
+}
+
